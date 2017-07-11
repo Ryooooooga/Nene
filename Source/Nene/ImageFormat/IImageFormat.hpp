1e2de03a5fd7c7f195fa382f5afdbd8dd6efcfb8
Ryooooooga <ryoga_314@yahoo.co.jp>
2017-07-11 22:16:30 +0900

    :new: Add image format interface.

---
+++ Source/Nene/ImageFormat/IImageFormat.hpp
@@ -0,0 +1,82 @@
+//=============================================================================
+// Copyright (c) 2017 Ryooooooga
+// https://github.com/Ryooooooga
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//=============================================================================
+
+#ifndef INCLUDE_NENE_IMAGEFORMAT_IIMAGEFORMAT_HPP
+#define INCLUDE_NENE_IMAGEFORMAT_IIMAGEFORMAT_HPP
+
+#include <array>
+#include <string>
+#include "../Types.hpp"
+#include "../Graphics/Image.hpp"
+#include "../Reader/IReader.hpp"
+
+namespace Nene
+{
+	/**
+	 * @brief      Image format interface.
+	 */
+	class IImageFormat
+	{
+	public:
+		/**
+		 * @brief      Constructor.
+		 */
+		IImageFormat() noexcept =default;
+
+		/**
+		 * @brief      Destructor.
+		 */
+		virtual ~IImageFormat() =default;
+
+		/**
+		 * @brief      Returns the name of the image format.
+		 *
+		 * @return     The name of the image format.
+		 */
+		[[nodiscard]]
+		virtual const std::string& name() const noexcept =0;
+
+		/**
+		 * @brief      Determines if the given data is an image header.
+		 *
+		 * @param[in]  header  The header byte data.
+		 *
+		 * @return     `true` if `header` seems the image header supported,
+		 *             `false` otherwise.
+		 */
+		[[nodiscard]]
+		virtual bool isImageHeader(const std::array<Byte, 16>& header) const noexcept =0;
+
+		/**
+		 * @brief      Constructs a image from a reader.
+		 *
+		 * @param      reader  The image data reader.
+		 *
+		 * @return     The image from `reader`.
+		 */
+		[[nodiscard]]
+		virtual Image read(IReader& reader) =0;
+	};
+}
+
+#endif  // #ifndef INCLUDE_NENE_IMAGEFORMAT_IIMAGEFORMAT_HPP
