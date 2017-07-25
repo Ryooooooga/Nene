159544976f2a7df9039e0a6d1bcafc78da3a7160
Ryooooooga <ryoga_314@yahoo.co.jp>
2017-07-25 18:33:13 +0900

    :ok: Add support for JPEG decompression.

--- Source/Nene/ImageFormat/JpegImageFormat.cpp
+++ Source/Nene/ImageFormat/JpegImageFormat.cpp
@@ -35,7 +35,15 @@ namespace Nene
 {
 	namespace
 	{
+		constexpr std::size_t inputBufferSize  = 4096;
 		constexpr std::size_t outputBufferSize = 4096;
+
+		struct SourceMgr
+		{
+			jpeg_source_mgr src;
+			IReader* reader;
+			JOCTET* buffer;
+		};
 
 		struct DestinationMgr
 		{
@@ -57,6 +65,49 @@ namespace Nene
 			throw JpegImageFormatException { buffer };
 		}
 
+		void initSrc([[maybe_unused]] j_decompress_ptr cinfo)
+		{
+		}
+
+		boolean fillInputBuffer(j_decompress_ptr cinfo)
+		{
+			const auto src = reinterpret_cast<SourceMgr*>(cinfo->src);
+			auto bytes = src->reader->read(src->buffer, inputBufferSize);
+
+			if (bytes == 0)
+			{
+				cinfo->err->msg_code = JERR_INPUT_EOF;
+				cinfo->err->error_exit(reinterpret_cast<j_common_ptr>(cinfo));
+			}
+
+			src->src.next_input_byte = src->buffer;
+			src->src.bytes_in_buffer = bytes;
+
+			return TRUE;
+		}
+
+		void skipInputData(j_decompress_ptr cinfo, long numBytes)
+		{
+			const auto src = reinterpret_cast<SourceMgr*>(cinfo->src);
+
+			if (numBytes > 0)
+			{
+				while (numBytes > src->src.bytes_in_buffer)
+				{
+					numBytes -= static_cast<long>(src->src.bytes_in_buffer);
+
+					fillInputBuffer(cinfo);
+				}
+
+				src->src.next_input_byte += static_cast<std::size_t>(numBytes);
+				src->src.bytes_in_buffer -= static_cast<std::size_t>(numBytes);
+			}
+		}
+
+		void termSrc([[maybe_unused]] j_decompress_ptr cinfo)
+		{
+		}
+
 		void initDest(j_compress_ptr cinfo)
 		{
 			const auto dest = reinterpret_cast<DestinationMgr*>(cinfo->dest);
@@ -68,10 +119,10 @@ namespace Nene
 			dest->dest.free_in_buffer   = outputBufferSize;
 		}
 
-		boolean outputBuffer(j_compress_ptr cinfo)
+		boolean emptyOutputBuffer(j_compress_ptr cinfo)
 		{
 			const auto dest = reinterpret_cast<DestinationMgr*>(cinfo->dest);
-		
+
 			// Write data.
 			if (dest->writer->write(dest->buffer, outputBufferSize) != outputBufferSize)
 			{
@@ -129,7 +180,66 @@ namespace Nene
 
 	Image JpegImageFormat::decode(IReader& reader)
 	{
-		return Image(100, 100);
+		jpeg_decompress_struct cinfo;
+		jpeg_error_mgr err;
+
+		// Set error handler.
+		cinfo.err          = jpeg_std_error(&err);
+		err.output_message = outputMessage;
+		err.error_exit     = error;
+
+		// Initialize decompression struct.
+		jpeg_create_decompress(&cinfo);
+
+		[[maybe_unused]] const auto _ = scopeExit([&]()
+		{
+			// Destroy decompression struct.
+			jpeg_destroy_decompress(&cinfo);
+		});
+
+		// Set source.
+		const auto src = static_cast<SourceMgr*>(cinfo.mem->alloc_small(
+			reinterpret_cast<j_common_ptr>(&cinfo), JPOOL_PERMANENT, sizeof(SourceMgr)));
+
+		src->buffer = static_cast<JOCTET*>(cinfo.mem->alloc_small(
+			reinterpret_cast<j_common_ptr>(&cinfo), JPOOL_IMAGE, inputBufferSize * sizeof(JOCTET)));
+
+		cinfo.src = &src->src;
+		src->src.init_source       = initSrc;
+		src->src.fill_input_buffer = fillInputBuffer;
+		src->src.skip_input_data   = skipInputData;
+		src->src.resync_to_restart = jpeg_resync_to_restart; // Use default.
+		src->src.term_source       = termSrc;
+		src->src.bytes_in_buffer   = 0;
+		src->src.next_input_byte   = nullptr;
+		src->reader                = &reader;
+
+		// Read data.
+		jpeg_read_header(&cinfo, TRUE);
+		jpeg_start_decompress(&cinfo);
+
+		Image image { static_cast<Int32>(cinfo.image_width), static_cast<Int32>(cinfo.image_height) };
+		std::vector<JSAMPLE> line(cinfo.image_width * cinfo.output_components);
+
+		for (std::size_t y = 0; y < cinfo.output_height; y++)
+		{
+			auto p = line.data();
+
+			jpeg_read_scanlines(&cinfo, &p, 1);
+
+			for (std::size_t x = 0; x < cinfo.image_width; x++)
+			{
+				image.dataPointer()[x + y*cinfo.image_width].red   = p[x*3 + 0];
+				image.dataPointer()[x + y*cinfo.image_width].green = p[x*3 + 1];
+				image.dataPointer()[x + y*cinfo.image_width].blue  = p[x*3 + 2];
+				image.dataPointer()[x + y*cinfo.image_width].alpha = 255;
+			}
+		}
+
+		// Finish decompress.
+		jpeg_finish_decompress(&cinfo);
+
+		return image;
 	}
 
 	void JpegImageFormat::encode(const Image& image, IWriter& writer)
@@ -162,7 +272,7 @@ namespace Nene
 
 		cinfo.dest                     = &dest->dest;
 		dest->dest.init_destination    = initDest;
-		dest->dest.empty_output_buffer = outputBuffer;
+		dest->dest.empty_output_buffer = emptyOutputBuffer;
 		dest->dest.term_destination    = termDest;
 		dest->writer                   = &writer;
 
@@ -183,7 +293,7 @@ namespace Nene
 		for (std::size_t y = 0; y < cinfo.image_height; y++)
 		{
 			auto p = line.data();
-		
+
 			for (std::size_t x = 0; x < cinfo.image_width; x++)
 			{
 				const auto& c = image.data()[x + y*cinfo.image_width];
