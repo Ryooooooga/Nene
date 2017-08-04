19a8c7b420306c339b993752ed9c006222d5f03b
Ryooooooga <ryoga_314@yahoo.co.jp>
2017-08-04 16:25:25 +0900

    :new: Add CRC.

---
+++ Source/Nene/Hash/Crc32.hpp
@@ -0,0 +1,46 @@
+//=============================================================================
+// Copyright (c) 2017 Ryooooooga
+// https://github.com/Ryooooooga
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//=============================================================================
+
+#ifndef INCLUDE_NENE_HASH_CRC32_HPP
+#define INCLUDE_NENE_HASH_CRC32_HPP
+
+#include "../ArrayView.hpp"
+
+namespace Nene::Hash
+{
+	using Crc32Digest = UInt32;
+
+	/**
+	 * @brief      Calculates CRC-32.
+	 *
+	 * @param[in]  bytes  The byte array.
+	 * @param[in]  crc    The initial digest.
+	 *
+	 * @return     CRC-32.
+	 */
+	constexpr Crc32Digest crc32(ByteArrayView bytes, Crc32Digest crc = 0x00000000) noexcept;
+}
+
+#include "Crc32.inl.hpp"
+
+#endif  // #ifndef INCLUDE_NENE_HASH_CRC32_HPP
